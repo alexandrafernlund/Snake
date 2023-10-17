badf64370241fde748a53c37e09992bbda90e726
alexandrafernlund <alexandrafernlund@gmail.com>
2023-10-17 10:16:06 +0200

    Snake now moves.

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,13 +20,63 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty }, // if pos is empty show "empty" image
+            { GridValue.Snake, Images.Body }, // if Snake is in a pos show "body" image
+            { GridValue.Food, Images.Food } // if food is in pos show "food" image
+        };
+
+
         private readonly int rows = 15, columns = 15;
         private readonly Image[,] gridImages;
+        private GameState gameState;
 
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetupGrid();
+            gameState = new GameState(rows, columns);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+            await GameLoop();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+
+            switch (e.Key) // to change Snakes direction
+            {
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(100); // change this if you want game to move faster
+                gameState.Move();
+                Draw();
+            }
         }
 
         private Image[,] SetupGrid()
@@ -50,6 +100,24 @@ namespace Snake
             }
 
             return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        // to look at gamestate and reflect object by looping thorugh every grid pos:
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c]; // get grid value from current pos
+                    gridImages[r, c].Source = gridValToImage[gridVal]; // set image
+                }
+            }
         }
     }
 }
